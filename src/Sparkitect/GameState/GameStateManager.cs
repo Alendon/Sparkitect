64861edd699ccd4977ea46442130b756ae974934
Alendon <AlexanderdoPanick@livedo.de>
2025-11-17 15:26:19 +0100

    Implement entrypoint processing in GameStateManager
    - Add ProcessStateMethodAssociations to build method registry
    - Add ProcessStateMethodOrdering to collect ordering constraints
    - Call entrypoint processing in EnterRootState
    - Populate method wrappers and schedule indices from entrypoints

--- src/Sparkitect/GameState/GameStateManager.cs
+++ src/Sparkitect/GameState/GameStateManager.cs
@@ -45,6 +45,67 @@ internal sealed class GameStateManager : IGameStateManager, IGameStateManagerReg
     public void EnterRootState(ICoreContainer coreContainer)
     {
         Log.Information("Entering Root State");
+        _currentContainer = coreContainer;
+
+        // Process entrypoints to build method registry
+        ProcessStateMethodAssociations();
+        ProcessStateMethodOrdering();
+    }
+
+    private void ProcessStateMethodAssociations()
+    {
+        Log.Debug("Processing state method associations");
+
+        using var entrypointContainer = ModManager.CreateEntrypointContainer<StateMethodAssociation>(new OneOf.Types.All());
+
+        var associations = entrypointContainer.ResolveMany();
+
+        foreach (var association in associations)
+        {
+            var builder = new StateMethodAssociationBuilder();
+            association.Configure(builder);
+
+            var mappings = builder.Build();
+
+            foreach (var (key, wrapperType, schedule) in mappings)
+            {
+                var (parentId, methodKey, scheduleType) = key;
+
+                // Store wrapper type
+                _methodWrappers[(parentId, methodKey)] = wrapperType;
+
+                // Add to schedule index
+                if (!_methodsBySchedule.TryGetValue(scheduleType, out var list))
+                {
+                    list = new List<(Identification, string)>();
+                    _methodsBySchedule[scheduleType] = list;
+                }
+
+                list.Add((parentId, methodKey));
+            }
+        }
+
+        Log.Debug("Registered {Count} state methods across {ScheduleCount} schedules",
+            _methodWrappers.Count, _methodsBySchedule.Count);
+    }
+
+    private void ProcessStateMethodOrdering()
+    {
+        Log.Debug("Processing state method ordering");
+
+        using var entrypointContainer = ModManager.CreateEntrypointContainer<StateMethodOrdering>(new OneOf.Types.All());
+
+        var orderings = entrypointContainer.ResolveMany();
+
+        foreach (var ordering in orderings)
+        {
+            var constraintSet = new HashSet<OrderingEntry>();
+            ordering.ConfigureOrdering(constraintSet);
+
+            _orderingConstraints.AddRange(constraintSet);
+        }
+
+        Log.Debug("Registered {Count} ordering constraints", _orderingConstraints.Count);
     }
 
 
