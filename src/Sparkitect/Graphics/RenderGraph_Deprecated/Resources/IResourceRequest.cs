6a10eb5afa20f7858b95297f2c982f62a2807202
Alendon <AlexanderdoPanick@livedo.de>
2026-07-02 15:02:44 +0200

    Delete the deprecated render graph

--- src/Sparkitect/Graphics/RenderGraph_Deprecated/Resources/IResourceRequest.cs
+++
@@ -1,7 +0,0 @@
-using JetBrains.Annotations;
-
-namespace Sparkitect.Graphics.RenderGraph_Deprecated.Resources;
-
-/// <summary>Marker for declaration-shape requests routed to a resource manager.</summary>
-[PublicAPI]
-public interface IResourceRequest<TResource>;
